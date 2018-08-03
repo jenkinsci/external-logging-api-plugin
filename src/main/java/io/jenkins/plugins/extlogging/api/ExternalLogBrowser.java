67aca31fb96b87b7bab45b4c7e0addbec649524b
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-08-03 13:06:43 +0200

    Move the getLogFile() compatibility caching method to the External Logging API

--- src/main/java/io/jenkins/plugins/extlogging/api/ExternalLogBrowser.java
+++ src/main/java/io/jenkins/plugins/extlogging/api/ExternalLogBrowser.java
@@ -4,9 +4,14 @@ import jenkins.model.logging.LogBrowser;
 import jenkins.model.logging.Loggable;
 
 import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Base abstract class for External Log Browsers.
+ * This implementation also implements a convenience method for caching of files.
  * @author Oleg Nenashev
  * @since TODO
  */
@@ -14,5 +19,18 @@ public abstract class ExternalLogBrowser extends LogBrowser {
 
     public ExternalLogBrowser(@Nonnull Loggable loggable) {
         super(loggable);
+    }
+
+    //TODO: Push warnings to Telemetry API
+    //Pipeline: LOGGER.log(Level.WARNING, "Avoid calling getLogFile on " + this,
+    //        new UnsupportedOperationException());
+    @Override
+    public File getLogFile() throws IOException {
+        File f = File.createTempFile("deprecated", ".log", getOwner().getTmpDir());
+        f.deleteOnExit();
+        try (OutputStream os = new FileOutputStream(f)) {
+            overallLog().writeRawLogTo(0, os);
+        }
+        return f;
     }
 }
